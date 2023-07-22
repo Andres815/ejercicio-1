b6e6949733782a01cd9884659abb17312884abcc
Andres815 <63661526+Andres815@users.noreply.github.com>
2023-07-22 01:10:35 -0500

    Add files via upload

---
+++ tipodatos/Datos.java
@@ -0,0 +1,22 @@
+package tipodatos;
+
+public class Datos {
+
+public static void main(String[] args) {
+    
+    int number = 1;
+    double number1 = 2.1;
+    short number2 = 2;
+    float number4 = 4.3f;
+    char caracter1 = 'a';
+    String nombre = "Andres";
+
+    System.out.println(nombre);
+    System.out.println(number);
+    System.out.println(number1);
+    System.out.println(number2);
+    System.out.println(number4);
+    System.out.println(caracter1);
+
+}
+}
